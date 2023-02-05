a8c196f9c60fbed17c81d85ddfba0630ded47920
SleepyBread <jnroy27@gmail.com>
2023-02-04 19:19:16 -0500

    Added authentication and home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthModule } from '@auth0/auth0-angular';
 
 import { AppComponent } from './app.component';
+import { AuthButtonComponent } from "../auth/auth.component";
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        AuthButtonComponent,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        RouterModule.forRoot([
+          {path: '', component: HomeComponent},
+        ]),
+        AuthModule.forRoot({
+            domain: 'dev-0pkr4qp67l06ca8u.us.auth0.com',
+            clientId: 'jxsbyicasIOb3b1Re0Q5dGfLY9KJZEXR',
+            authorizationParams: {
+                redirect_uri: window.location.origin
+            }
+        }),
+    ]
 })
 export class AppModule { }
